c170e468ba33351e87a0170f7e04b3abcbcb9e8d
loay106 <30694922+loay106@users.noreply.github.com>
2019-11-14 22:50:22 +0200

    bug fixes

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ function retrieveTweets(T,parameters){
 	return T.get('search/tweets', parameters);
 }
 
-async function CalculateTMedian(term){
+async function twitterMedianLength(term){
 	var config = require('./config.js');
 	var Twit = require('twit');
 	var T = new Twit(config);
@@ -16,11 +16,15 @@ async function CalculateTMedian(term){
 	parameters.count = TWEETS_PER_REQUEST;
 	parameters.q = term;
 	var tweet_sizes = new Array(MAX_TWEETS);
+	var retrieving_error = false;
 	
 	for(let i=0; i<MAX_TWEETS/TWEETS_PER_REQUEST; i++){
 		await retrieveTweets(T,parameters).then(res => {
 			let lowest_id = null;
 			let tweets = res.data.statuses;
+			if(tweets.length < TWEETS_PER_REQUEST && TWEETS_PER_REQUEST*i+tweets.length < MAX_TWEETS){
+				throw "Not enough tweets";
+			}
 			for(let j=0;j<tweets.length; j++){
 				tweet_sizes[TWEETS_PER_REQUEST*i+j] = tweets[j].text.length;
 				if(!lowest_id || lowest_id>tweets[i].id){
@@ -29,16 +33,13 @@ async function CalculateTMedian(term){
 			}
 			parameters.max_id = lowest_id; 
 		}).catch(error => {
-			console.log(error);
+			retrieving_error = true;
 		});
+		if(retrieving_error){break;}
 	}
+	if(retrieving_error) return null;
 	tweet_sizes.sort();
 	return tweet_sizes[MAX_TWEETS/2+1];
-}
-
-async function twitterMedianLength(term){
-	var median_res = await CalculateTMedian(term);
-	return median_res;
 }
 
 module.exports.median = async function(event, context, callback){
@@ -49,7 +50,7 @@ module.exports.median = async function(event, context, callback){
 	}
 	let median_res = await twitterMedianLength(term);
 	if(!median_res){
-		// error connecting to Twitter API
+		// error connecting to Twitter API or retrieving tweets
 		callback(null, null);
 	}
 	callback(null, JSON.stringify(median_res));
